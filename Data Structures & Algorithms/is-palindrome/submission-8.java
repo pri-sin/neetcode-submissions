b9c37dbc17383246943bde6841b39f6e7b3fac32
pri-sin <priyasingh30.official@gmail.com>
2026-08-07 20:51:31 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0, right=s.length()-1;
+
+        while(left<right){
+            while(left<s.length() && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(right>=0 && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(left<=right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
