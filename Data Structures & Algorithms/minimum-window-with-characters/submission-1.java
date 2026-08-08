505f68e3fd9f9dbdc544e12585120b1397626f3c
pri-sin <priyasingh30.official@gmail.com>
2026-08-08 23:51:24 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.java
@@ -0,0 +1,92 @@
+/*class Solution {
+    public String minWindow(String s, String t) {
+        int shortest=Integer.MAX_VALUE;
+        HashMap<Character,Integer> map=new HashMap<>();
+        //create a map of all the characters in string t
+        for(int i=0;i<t.length();i++){
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+        }
+        int count=t.length();
+
+        int left=0,right=0, start=0;
+        while(right<s.length()){
+            //extract character from right
+            char c=s.charAt(right);
+
+            //check if it is there in the map
+            if(map.containsKey(c)){
+                //decrement count if yes
+                if(map.get(c)>0){
+                    count--;
+                }
+                map.put(c, map.getOrDefault(c,0)-1);
+                
+            }
+            
+            while(count==0){
+                if(shortest>(right-left+1)){
+                    shortest=right-left+1;
+                    start=left;
+                }
+
+                char l=s.charAt(left);
+                if(map.containsKey(l)){
+                    map.put(l, map.getOrDefault(l,0)+1);
+                    if(map.get(l)>0)
+                    count++;
+                }
+                left++;
+            }
+            right++;
+        }
+        return shortest==Integer.MAX_VALUE?"":s.substring(start,start+shortest);
+    }
+}*/
+
+class Solution {
+    public String minWindow(String s, String t) {
+        int left=0, right=0;
+        Map<Character, Integer> map=new HashMap<>();
+
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        int count=t.length();
+        int start=0;
+        int minlen=Integer.MAX_VALUE;
+        while(right<s.length()){
+            char c = s.charAt(right);
+
+            if(map.containsKey(c)){
+                // Only decrement count if we still need this instance of character c
+                if (map.get(c) > 0) {
+                    count--;
+                }
+                map.put(c,map.getOrDefault(c,0)-1);
+            }
+
+            while(count<=0){
+                if(count==0){
+                    if(right-left+1<minlen){
+                        minlen = right-left+1;
+                        start=left;
+                    }
+                }
+                char r= s.charAt(left);
+                left++;
+                if(map.containsKey(r)){
+                    map.put(r,map.getOrDefault(r,0)+1);
+                    // Only increment count if we are now missing a required character
+                    if (map.get(r) > 0) {
+                        count++;
+                    }
+                }
+            }
+            right++;
+        }
+        return minlen==Integer.MAX_VALUE?"":s.substring(start, start+minlen);
+
+    }
+}
