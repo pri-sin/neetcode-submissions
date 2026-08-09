552d7db83073c7c8b79e81e7afec524810e01bb8
pri-sin <priyasingh30.official@gmail.com>
2026-08-09 16:35:17 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m =matrix.length;
+        int n =matrix[0].length;
+
+        int l=0;
+        int r=m*n-1;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+            int midval=matrix[mid/n][mid%n];
+
+            if(midval==target){
+                return true;
+            }
+
+            if(midval<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return false;
+    }
+}
