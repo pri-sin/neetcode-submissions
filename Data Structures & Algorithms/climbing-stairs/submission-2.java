9c22b5f1637ebdaa620de753ab5a3af9f1512ab6
pri-sin <priyasingh30.official@gmail.com>
2026-08-28 09:04:45 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,44 @@
+/*class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        return dynamo(dp, n);
+    }
+
+    public int dynamo(int[] dp, int i) {
+        // Base cases: 
+        if (i < 0) return 0; // Out of bounds prevention
+        if (i == 0 || i == 1) return 1; // 1 way to stay at step 0 or step 1
+        
+        // If already calculated, return the cached value
+        if (dp[i] != 0) {
+            return dp[i];
+        }
+
+        // Memoize the result
+        dp[i] = dynamo(dp, i - 1) + dynamo(dp, i - 2); 
+        return dp[i];
+    }
+}*/
+
+class Solution {
+
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        return dynamo(dp, n);
+    }
+
+    public int dynamo(int[] dp, int i) {
+        // Base cases: 
+        if (i < 0) return 0; 
+        if (i == 0 ) return 1; 
+        
+        // If already calculated, return the cached value
+        if (dp[i] != 0) {
+            return dp[i];
+        }
+
+        // Memoize the result
+        dp[i] = dynamo(dp, i - 1) + dynamo(dp, i - 2); 
+        return dp[i];
+    }
+}
